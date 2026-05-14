8e013992d75354547fdc2b6c12475938d5506c39
Bruno Filipe Silva Lourenço <1221911@isep.ipp.pt>
2026-05-14 16:57:01 +0100

    G - project setup - Spring Boot, Docker, CI pipeline

---
+++ backend/src/main/java/com/kryptos/KryptosApplication.java
@@ -0,0 +1,11 @@
+package com.kryptos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KryptosApplication.class, args);
+    }
+}
