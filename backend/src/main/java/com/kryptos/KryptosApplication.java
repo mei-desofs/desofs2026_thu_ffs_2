a55445accbb4e73f4fdd16f08ac177b223e66f8a
Bruno Filipe Silva Lourenço <1221911@isep.ipp.pt>
2026-06-15 23:02:10 +0100

    I - #22 - implement log transmission to external systems

--- backend/src/main/java/com/kryptos/KryptosApplication.java
+++ backend/src/main/java/com/kryptos/KryptosApplication.java
@@ -2,8 +2,10 @@ package com.kryptos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KryptosApplication {
     public static void main(String[] args) {
         SpringApplication.run(KryptosApplication.class, args);
